75397faf4398d189c056353f3503a47db2901138
Brian Capouch <brianc@palaver.net>
2016-04-15 01:50:24 -0400

    Remove cruft from early work

--- js/sample-page.jsx
+++ js/sample-page.jsx
@@ -21,60 +21,6 @@ import DropZoneComponent from 'react-dropzone-component'
 import Upload from './Upload'
 import Browse from './Browse'
 
-// Shims, for now:
-let
-  // Configuration and setup for DropZoneComponent
-  componentConfig = {
-    iconFiletypes: ['.jpg', '.png', '.gif', 'tif'],
-    showFiletypeIcon: true,
-    postUrl: '/uploadHandler'
-  },
-  eventHandlers = {
-    // This one receives the dropzone object as the first parameter
-    // and can be used to additional work with the dropzone.js
-    // object
-    init: null,
-    // All of these receive the event as first parameter:
-    drop: null,
-    dragstart: null,
-    dragend: null,
-    dragenter: null,
-    dragover: null,
-    dragleave: null,
-    // All of these receive the file as first parameter:
-    addedfile: null,
-    removedfile: null,
-    thumbnail: null,
-    error: null,
-    processing: null,
-    uploadprogress: null,
-    sending: null,
-    success: null,
-    complete: null,
-    canceled: null,
-    maxfilesreached: null,
-    maxfilesexceeded: null,
-    // All of these receive a list of files as first parameter
-    // and are only called if the uploadMultiple option
-    // in djsConfig is true:
-    processingmultiple: null,
-    sendingmultiple: null,
-    successmultiple: null,
-    completemultiple: null,
-    canceledmultiple: null,
-    // Special Events
-    totaluploadprogress: null,
-    reset: null,
-    queuecomplete: null
-    },
-    djsConfig = {
-      addRemoveLinks: true,
-      acceptedFiles: "image/jpeg,image/png,image/gif,image/tiff"
-    };
-
-// end local variables
-
-
 const brandName = "Scene:History";
 const brand = <span>{brandName}</span>;
 
